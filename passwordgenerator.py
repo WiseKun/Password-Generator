705f7589540e87ce09e76c8a78799bf24f574ea9
WiseKun <66040816+WiseKun@users.noreply.github.com>
2021-02-23 10:26:38 +0000

    Improve average strength

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,32 +1,47 @@
-# Password Generator made by Mustafa A
-# Version 1.0 -- Add basic functionality
-# Version 1.1 -- Make passwords a bit shorter + Remove quantity + Add password strength + Able to see average password strength
-
+# Password Generator made by Mustafa A (WiseKun)
 import random
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 alphabet_len = len(alphabet)
 
-other_characters = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '£', '$', '%', '&', ':', ';', '@', '#', '~', '<', '>', '?']
+other_characters = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '£', '$', '%', '&', '@', '#', '~', '<', '>', '?']
 other_characters_len = len(other_characters)
 
 def generate_password():
     password_lst = []
-    for i in range(random.randint(8, 12)):
+    num_of_upper_letters = random.randint(3, 4)
+    num_of_lower_letters = random.randint(3, 4)
+    num_of_numbers = random.randint(3, 4)
+    num_of_other_characters = random.randint(3, 4)
+    length = num_of_upper_letters+num_of_lower_letters+num_of_numbers+num_of_other_characters
+
+    while (len(password_lst) < length):
         random1 = random.randint(0, 1)
-        if (random1==0):
+        if (random1 == 0) and (num_of_lower_letters >= 1 or num_of_upper_letters >= 1):
+            #print('letter')
             random2 = random.randint(0, 1)
-            if (random2==0):
+            if (random2 == 0) and (num_of_lower_letters > 0):
+                #print('lower')
+                num_of_lower_letters -= 1
                 password_lst.append(alphabet[random.randint(0, alphabet_len-1)])
-            else:
+            elif (random2 == 1) and (num_of_upper_letters > 0):
+                #print('upper')
+                num_of_upper_letters -= 1
                 password_lst.append(alphabet[random.randint(0, alphabet_len-1)].upper())
-        else:
-            password_lst.append(other_characters[random.randint(0, other_characters_len-1)])
+                
+        elif (random1 == 1) and (num_of_numbers >= 1 or num_of_other_characters >= 1):
+            random2 = random.randint(0, 1)
+            if (random2 == 0) and (num_of_numbers > 0):
+                num_of_numbers -= 1
+                password_lst.append(other_characters[random.randint(0, 10)])
+            elif (random2 == 1) and (num_of_other_characters > 0):
+                num_of_other_characters -= 1
+                password_lst.append(other_characters[random.randint(10, other_characters_len-1)])
 
     return ''.join(password_lst)
 
-password = generate_password()
-print('{}'.format(password))
+my_password = generate_password()
+print('{}'.format(my_password))
 
 def password_strength(password):
     password_check = {'lower': False, 'upper': False, 'number': False, 'other': False, 'length': False}
@@ -45,24 +60,21 @@ def password_strength(password):
     
     strong_password = True
     for value in password_check.values():
-        if (value==False):
+        if (value == False):
             strong_password = False
             break
         else:
             pass
-    return strong_password
+    return strong_password 
+    # If the password is not strong it returns 0, else, it returns 1
 
-passwords = []
-for i in range(1000):
-    passwords.append(generate_password())
+def determine_strength(quantity=1000):
+    total_strength = 0
+    for i in range(quantity):
+        password = generate_password()
+        strength = password_strength(password)
+        #print("Password: {}\nStrength: {}\n".format(password, strength))
+        total_strength += strength
+    return float(round(total_strength/quantity))
 
-strengths = []
-for password in passwords:
-    strengths.append(password_strength(password))
-
-total_strength = 0
-for strength in strengths:
-    total_strength += strength
-
-average_strength = total_strength/len(strengths)
-print(average_strength) # The current average strength is 0.7 to 0.8 most of the time
+print(determine_strength(1000)) # The current average strength is always 1.0
